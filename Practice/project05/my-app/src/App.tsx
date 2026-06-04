e2906f8d8ace9162c4fd85a5e75aae956c37d196
= <mayankrathore9897@gmail.com>
2026-06-04 11:41:54 +0530

    shadcn setup is done

--- Practice/project05/my-app/src/App.tsx
+++ Practice/project05/my-app/src/App.tsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
-type Product={
-    id:number,
-    name:string,
-    price:number,
+import { Button } from "@/components/ui/button";
+
+type Product = {
+    id: number,
+    name: string,
+    price: number,
 }
 type CartItem = {
     id: number;
@@ -12,37 +14,40 @@ type CartItem = {
 };
 
 const App = () => {
-   const [cart,setCart] = useState<CartItem[]>([])
-   const addToCart=(product:Product)=>{
-    const existingItem=cart.find(item=>item.id===product.id);
-    if(existingItem){
-        setCart(cart.map(item=>item.id===product.id?{...item,quantity:item.quantity+1}:item))
-    } else {
-        setCart([...cart, { ...product, quantity: 1 }]);
+    const [cart, setCart] = useState<CartItem[]>([])
+    const addToCart = (product: Product) => {
+        const existingItem = cart.find(item => item.id === product.id);
+        if (existingItem) {
+            setCart(cart.map(item => item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item))
+        } else {
+            setCart([...cart, { ...product, quantity: 1 }]);
+        }
     }
-   }
-   const removeItem=(id:number)=>{
-    setCart(cart.filter(item=>item.id!==id))
-   }
-   const IncreaseQuantity=(id:number)=>{
-     setCart(cart.map(item=>item.id===id ? {...item,quantity:item.quantity+1} : item))
-   }
-   const DecreaseQuantity =(id:number)=>{
-    setCart(cart.map(item=>item.id=== id ? {...item,qunatity:item.quantity-1}:item))
-   }
-   const total = cart.reduce((sum,item)=>
-    sum+item.price *item.quantity,
-   0
-  )
-  const CartBadgeCount = cart.reduce((sum,item)=>
-  sum+item.quantity,
-  0
-)
+    const removeItem = (id: number) => {
+        setCart(cart.filter(item => item.id !== id))
+    }
+    const IncreaseQuantity = (id: number) => {
+        setCart(cart.map(item => item.id === id ? { ...item, quantity: item.quantity + 1 } : item))
+    }
+    const DecreaseQuantity = (id: number) => {
+        setCart(cart.map(item => item.id === id ? { ...item, qunatity: item.quantity - 1 } : item))
+    }
+    const total = cart.reduce((sum, item) =>
+        sum + item.price * item.quantity,
+        0
+    )
+    const CartBadgeCount = cart.reduce((sum, item) =>
+        sum + item.quantity,
+        0
+    )
     return (
         <>
+            <div>
 
+            </div>
+            <Button onclick={() => addToCart(product)}>Add to Cart</Button>
         </>
-       
+
     );
 };
 
