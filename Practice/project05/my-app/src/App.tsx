3dbec9f38ba78d2f3c102f5a421a552969c81421
= <mayankrathore9897@gmail.com>
2026-06-06 01:17:29 +0530

    designing ui component of shadcn

--- Practice/project05/my-app/src/App.tsx
+++ Practice/project05/my-app/src/App.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
-import { Button } from "@/components/ui/button";
+import { Button } from "./components/ui/button";
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "./components/ui/sheet"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from "./components/ui/card"
 
 type Product = {
     id: number,
@@ -42,10 +44,64 @@ const App = () => {
     )
     return (
         <>
-            <div>
+            <div className=" bg-gray-800 min-h-screen ">
+                <div className="min-h-screen  flex  items-center justify-center">
+                    <Card className="  w-full max-w-sm">
+                        <CardHeader>
+                            <CardTitle className="text-center">Product Card</CardTitle>
+                            <CardDescription>
+                                This card uses the small size variant.
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <p>
+                                The card component supports a size prop that can be set to
+                                &quot;sm&quot; for a more compact appearance.
+                            </p>
+                        </CardContent>
+                        <CardFooter className="flex gap-3">
+                            <Button variant="outline" onClick={() => addToCart(product)} className="px-9 py-6 text-xl rounded-xl bg-gray-600 hover:bg-gray-500 cursor-pointer text-white">
+                                Add to Cart
+                            </Button>
+                            <Sheet>
+                                <SheetTrigger asChild>
+                                    <Button variant="outline" className="px-9 py-6 text-xl rounded-xl bg-gray-600 hover:bg-gray-500 cursor-pointer  text-white">Your Cart</Button>
+                                </SheetTrigger>
+                                <SheetContent showCloseButton={false}>
+                                    <div>
+                                        {
+                                            cart.length === 0 ?
+                                                <p className="text-center font-semibold ">No Cart...</p>
+                                                : (
+                                                    cart.map((item) => (
+                                                        <div className="" key={item.id}>
+                                                            <p>
+                                                                {item.name}
+                                                            </p>
+                                                            <p>
+                                                                {item.quantity}
+                                                            </p>
+                                                            <p>
+                                                                {item.price}
+                                                            </p>
+                                                            <div className="flex gap-3">
+                                                                <Button onClick={() => IncreaseQuantity(item.id)}>+</Button>
+                                                                <Button onClick={() => DecreaseQuantity(item.id)}>-</Button>
+                                                            </div>
+                                                        </div>
 
+                                                    ))
+                                                )
+                                        }
+                                    </div>
+                                </SheetContent>
+                            </Sheet>
+                        </CardFooter>
+                    </Card>
+                </div>
             </div>
-            <Button onclick={() => addToCart(product)}>Add to Cart</Button>
+
+
         </>
 
     );
