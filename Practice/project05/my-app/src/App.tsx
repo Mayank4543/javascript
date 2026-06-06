245d664dd761a9c893a58503ad2f27b5c3a51d29
= <mayankrathore9897@gmail.com>
2026-06-06 13:38:53 +0530

    completing this modulew

--- Practice/project05/my-app/src/App.tsx
+++ Practice/project05/my-app/src/App.tsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Button } from "./components/ui/button";
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "./components/ui/sheet"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from "./components/ui/card"
+import { Sheet, SheetContent, SheetTrigger } from "./components/ui/sheet"
+import { Card, CardContent, CardFooter, CardHeader, CardTitle, } from "./components/ui/card"
 
 type Product = {
     id: number,
@@ -29,10 +29,14 @@ const App = () => {
         setCart(cart.filter(item => item.id !== id))
     }
     const IncreaseQuantity = (id: number) => {
-        setCart(cart.map(item => item.id === id ? { ...item, quantity: item.quantity + 1 } : item))
+        const updatedValues = cart.map(item => item.id === id ? { ...item, quantity: item.quantity + 1 } : item)
+        console.log("bbcds",updatedValues)
+        
+        setCart(updatedValues)
     }
     const DecreaseQuantity = (id: number) => {
-        setCart(cart.map(item => item.id === id ? { ...item, qunatity: item.quantity - 1 } : item))
+        console.log("nbfdksfnsd", id)
+        setCart(cart.map(item => item.id === id ? { ...item, quantity: item.quantity>1?item.quantity-1:1 } : item))
     }
     const total = cart.reduce((sum, item) =>
         sum + item.price * item.quantity,
@@ -42,6 +46,12 @@ const App = () => {
         sum + item.quantity,
         0
     )
+    const product: Product = {
+        id: 1,
+        "name": "Guitar",
+        "price": 500
+
+    }
     return (
         <>
             <div className=" bg-gray-800 min-h-screen ">
@@ -49,15 +59,16 @@ const App = () => {
                     <Card className="  w-full max-w-sm">
                         <CardHeader>
                             <CardTitle className="text-center">Product Card</CardTitle>
-                            <CardDescription>
-                                This card uses the small size variant.
-                            </CardDescription>
+                            
                         </CardHeader>
                         <CardContent>
                             <p>
-                                The card component supports a size prop that can be set to
-                                &quot;sm&quot; for a more compact appearance.
+                               {product.name}
                             </p>
+                            <p>
+                                {product.price}
+                            </p>
+
                         </CardContent>
                         <CardFooter className="flex gap-3">
                             <Button variant="outline" onClick={() => addToCart(product)} className="px-9 py-6 text-xl rounded-xl bg-gray-600 hover:bg-gray-500 cursor-pointer text-white">
@@ -74,20 +85,23 @@ const App = () => {
                                                 <p className="text-center font-semibold ">No Cart...</p>
                                                 : (
                                                     cart.map((item) => (
-                                                        <div className="" key={item.id}>
+                                                        <div className="" key={item.id} >
                                                             <p>
-                                                                {item.name}
+                                                               Product Name: {item.name}
                                                             </p>
                                                             <p>
-                                                                {item.quantity}
+                                                              Product Quantity:  {item.quantity}
                                                             </p>
                                                             <p>
-                                                                {item.price}
+                                                               Price: {item.price *item.quantity}
                                                             </p>
-                                                            <div className="flex gap-3">
-                                                                <Button onClick={() => IncreaseQuantity(item.id)}>+</Button>
-                                                                <Button onClick={() => DecreaseQuantity(item.id)}>-</Button>
+                                                            <div className="flex gap-2">
+                                                                <Button onClick={() => IncreaseQuantity(item.id)} className="px-6 py-5 font-semibold">+</Button>
+                                                                <Button onClick={() => DecreaseQuantity(item.id)} className="px-6 py-5 font-semibold">-</Button>
+                                                                <Button onClick={() => removeItem(item.id)} className="px-6 py-5 font-semibold rounded-xl">Remove</Button>
+
                                                             </div>
+                                                            <p className="text-green-300 py-px">Total Price:{total}</p>
                                                         </div>
 
                                                     ))
